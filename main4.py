d9249d70bb9890fe93dcf959cb11525452724168
777 <goszakupki.aktiv@gmail.com>
2023-01-03 01:28:57 +0300

    lesson 4 /start /help

---
+++ main4.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, types, executor
+
+HELP_COMMAND = """
+/help - all commands
+
+/start - we are start 
+"""
+
+TOKEN_API ="5724701366:AAFEQHc0elthrFpat_eSY6PJIK_zIiEdqMA" #токен
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["help"])
+async def help1(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
+
+
+@dp.message_handler(commands=["start"])
+async def help2(message: types.Message):
+    await message.answer(text="You are welcom to our Bot")
+    await message.delete();
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)

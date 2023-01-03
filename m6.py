2cd363cfc428e2dc268290079e5babdc6fdcfa7e
777 <goszakupki.aktiv@gmail.com>
2023-01-03 19:04:07 +0300

    урок 6 домашка Бот возвращает id стикера
    @dp.message_handler(content_types=["sticker"])
    async def st10(message: types.Message):
        await message.answer(message.sticker.file_id)

--- m6.py
+++ m6.py
@@ -8,19 +8,34 @@ bot = Bot(ID_MY)
 
 dp = Dispatcher(bot)
 
+HELP_COMMAND = """
+
+<b>/help  </b> - lol
+/start - hello
+<em>/mem</em>     - просто мем
+"""
+
+
 async def lol_on_startup(_):
     print("Bot is start!!!")
 
 
+@dp.message_handler(commands=["help"])
+async def help66(message: types.Message):
+    await message.reply(HELP_COMMAND, parse_mode="html")
+
+@dp.message_handler(content_types=["sticker"])
+async def st10(message: types.Message):
+    await message.answer(message.sticker.file_id)
+
+
+
 @dp.message_handler()
-async def vvv(message: types.Message):
-    if message.text.__contains__("✅"):
-        message.text = set(string)
-        summ = []
-        await message.answer("В Вашем сообщении знак ✅ встречается" )
+async def count22(message: types.Message):
+    await message.answer(text=str(message.text.count("✅")))
 
 @dp.message_handler(commands=["give"])
-async def giv333(message: types.Message):
+async def give333(message: types.Message):
     await message.answer("<b>Смотри</b>, <em>какой смешной кот</em>" + '❤️', parse_mode="html")
     await bot.send_sticker(message.from_user.id, sticker="CAACAgIAAxkBAAEHGxhjs46hE0TzSmzRwbqZsZdvQaBWvwACxgoAAhf-gUojb09iakYCMC0E")
 

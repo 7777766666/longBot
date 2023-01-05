4a8bf098f95ef1e4d29a83dc85713ad46f255f22
777 <goszakupki.aktiv@gmail.com>
2023-01-05 19:49:02 +0300

    14 lesson p.py готово

--- p.py
+++ p.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, Dispatcher, executor, types
 from config import PP777Bot
-from p_keyboards import kb
-
+from p_keyboards import kb, kb_photo
+from aiogram.dispatcher.filters import Text
+import random
 
 bot = Bot(token=PP777Bot) # создаем экземпляр класса Бот
 dp = Dispatcher(bot=bot)  #передаем в диспетчер экземпляр бота
@@ -10,10 +11,39 @@ HELP_COMMAND = """"
 <b>/help - так и быть, помогу</b>
 <em>/s - начало положено!</em>
 <b>/sticker - пришлю стике</b>
+/Random - test!!!!
 """
 
 async def start777(_):  #запускается экзекьютером при запуске, чтоб было видно, что блот стартовал
     print("start777")
+
+arr_photos = [
+    "https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-01.jpg",
+    "https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-03.jpg",
+    "https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-09.jpg"
+
+]
+
+
+
+@dp.message_handler(Text(equals="Random photo"))
+async def open_kb_photo(message: types.Message):
+    await message.answer(text="Random photo enter Random",
+                         reply_markup=kb_photo)
+    await message.delete()
+
+@dp.message_handler(Text(equals="Random"))
+async def get_randon1(message: types.Message):
+    await bot.send_photo(chat_id=message.chat.id,
+                         photo=random.choice(arr_photos))
+    await message.delete()
+
+
+@dp.message_handler(Text(equals="Main manu"))
+async def get_main_manu(message: types.Message):
+    await message.answer(text="Return to main",
+                         reply_markup=kb)
+    await message.delete()
 
 @dp.message_handler(commands=["s"])
 async def command_start(message: types.Message):
@@ -30,6 +60,11 @@ async def command_desc(message: types.Message):
     await message.answer(text="I super Bot and I can send random sticker")
     await bot.send_sticker(chat_id=message.chat.id, sticker="CAACAgIAAxkBAAEHIJNjtiALX3BGX9cKleaFaa9CHHx7KAACUQADwDZPExTRqeEWVGi2LQQ")
     await message.delete()
+
+
+@dp.message_handler(commands=["lol"])
+async def test1(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text="В личку должен написать сообщение")
 
 
 
--- p_keyboards.py
+++ p_keyboards.py
@@ -4,5 +4,11 @@ kb = ReplyKeyboardMarkup(resize_keyboard=True)  #клавиатура подст
 bt1 = KeyboardButton(text="/help")
 bt2 = KeyboardButton(text="/s")
 bt3 = KeyboardButton(text="/sticker")
+bt4 = KeyboardButton(text="Random photo")
 
-kb.add(bt1, bt2).add(bt3)
+kb.add(bt1, bt2).add(bt3, bt4)
+kb_photo = ReplyKeyboardMarkup(resize_keyboard=True)
+bp1 = KeyboardButton(text="Random")
+bp2 = KeyboardButton(text="Main manu")
+
+kb_photo.add(bp1, bp2)

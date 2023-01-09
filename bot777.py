204948651596d84b68609001e06ca47cd2414624
777 <goszakupki.aktiv@gmail.com>
2023-01-09 22:29:16 +0300

    40 lesson
    bot777

--- bot777.py
+++ bot777.py
@@ -115,4 +115,4 @@ async def load_description(mess: types.Message, state: FSMContext) -> None:
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    executor.start_polling(dp)

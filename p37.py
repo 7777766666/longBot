df8705a31837cdc88b8ec7dbbc45e71a5007c166
777 <goszakupki.aktiv@gmail.com>
2023-01-09 05:55:08 +0300

    40 lesson
    Анкета бот готовая часть
    финал

--- p37.py
+++ p37.py
@@ -22,27 +22,50 @@ def get_kb() -> ReplyKeyboardMarkup:
     return kb
 
 
+def get_cancel_kb() -> ReplyKeyboardMarkup:
+    kb2 = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb2.add(KeyboardButton("/cancel"))
+    return kb2
+
+
 async def start777(_):
     print("KENGURY")
 
 
+@dp.message_handler(commands=["cancel"], state="*")
+async def cmd_cancel1(mess: types.Message, state: FSMContext):
+    if state is None:
+        return
+
+    await state.finish()
+    await mess.reply(text="Вы прервали заполнение профиля!",
+                     reply_markup=get_kb())
+
+
 @dp.message_handler(commands=["s"])
 async def cmd_start(mess: types.Message) -> None:
-    await mess.answer(text="welcome, enter your name /create",
+    await mess.answer(text="Желаете заполнить Ваш профиль /create",
                       reply_markup=get_kb())
 
 
 @dp.message_handler(commands=["create"])
 async def cmd_create(mess: types.Message) -> None:
-    await mess.reply(text="Давайте оформим Ваш профиль. Пришлит своё фото.")
+    await mess.reply(text="Давайте оформим Ваш профиль. Пришлит своё фото.",
+                     reply_markup=get_cancel_kb())
     await ProfileStatesGroup.photo.set()    #устанавливаем состояние на ожидание фото
 
+
+@dp.message_handler(lambda message: not message.photo, state=ProfileStatesGroup.photo)
+async def check_photo(mess: types.Message):
+    await mess.reply("Это не фотография!")
+
+
 @dp.message_handler(content_types=["photo"], state=ProfileStatesGroup.photo)
-async def load_photo(mess: types.Message, state:FSMContext) -> None:
+async def load_photo(mess: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data["photo"] = mess.photo[0].file_id
 
-    await mess.reply(text="Как Ваше имя?")
+    await mess.reply(text="Введите Ваше имя")
     await ProfileStatesGroup.next()
 
 
@@ -53,6 +76,11 @@ async def load_name(mess: types.Message, state: FSMContext) -> None:
 
     await mess.answer(text="Сколько Вам лет?")
     await ProfileStatesGroup.next()
+
+
+@dp.message_handler(lambda mess: not mess.text.isdigit() or float(mess.text) > 90, state=ProfileStatesGroup.age)
+async def check_age(mess: types.Message):
+    await mess.answer(text="Введите Ваш реальный возраст (число)")
 
 
 @dp.message_handler(state=ProfileStatesGroup.age)
@@ -70,7 +98,7 @@ async def load_description(mess: types.Message, state: FSMContext) -> None:
         data["description"] = mess.text
         await bot.send_photo(chat_id=mess.chat.id,
                              photo=data["photo"],
-                             caption=f"{data['name']}, {data['age']}\n{data['description']} ")
+                             caption=f"Ваше имя: {data['name']}\nВам: {data['age']} лет\nЗадача: {data['description']} ")
 
     await mess.reply(text="Спасибо, Ваш профиль полностью заполнен!")
     await state.finish()
